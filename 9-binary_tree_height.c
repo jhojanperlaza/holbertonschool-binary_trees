f8330f92fcddfdfe29c36e644db3856afe92c76c
jhojan <jhojan_perlaza123>
2022-07-27 20:18:57 -0500

    [UPDATED] file

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,9 +11,6 @@ size_t height_recursion(const binary_tree_t *tree)
 	size_t cont_left = 0;
 	size_t cont_right = 0;
 
-	if (tree == NULL)
-		return (0);
-
 	cont_left = 1 + height_recursion(tree->left);
 	cont_right = 1 + height_recursion(tree->right);
 
@@ -32,6 +29,9 @@ size_t height_recursion(const binary_tree_t *tree)
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t result = 0;
+
+	if (tree == NULL)
+		return (0);
 
 	result = height_recursion(tree);
 	return (result - 1);
