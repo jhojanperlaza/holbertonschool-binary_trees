09378f7d85e3125e18ff5ad427f500f663e29e7b
jhojan <jhojan_perlaza123>
2022-07-28 11:35:41 -0500

    [ADD] file 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -10,11 +10,11 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return 0;
-	int result = 0;
-	if ((tree->left == NULL && tree->right != NULL) || (tree->left != NULL && tree->right == NULL))
-	{
-		result++;
-	}
-	result += (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
-	return result;
+
+	if (tree->left != NULL && tree->right == NULL)
+		return binary_tree_nodes(tree->left) + 1;
+	else if (tree->left == NULL && tree->right != NULL)
+		return binary_tree_nodes(tree->right) + 1;
+
+	return binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
 }
